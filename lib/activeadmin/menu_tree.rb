72b602143106735f6a4961182edce0a8eefda923
shuuuuuny <shuuuuuny@gmail.com>
2021-09-05 19:13:37 +0900

    apply on ActiveAdmin.before_load

--- lib/activeadmin/menu_tree.rb
+++ lib/activeadmin/menu_tree.rb
@@ -21,3 +21,35 @@ module ActiveAdmin
     end
   end
 end
+
+ActiveAdmin.before_load do |config|
+  ActiveAdmin::DSL.prepend ActiveAdmin::MenuTree::DSL
+
+  menu_tree_config = ActiveAdmin::MenuTree.config.menu_tree
+
+  config.namespace :admin do |admin|
+    admin.build_menu do |menu|
+      menu_tree_config.each.with_index(1) do |item, index|
+        options = item.except(:children)
+        options[:label] ||= item[:name]
+        # pp options
+        menu.add priority: index * 10, **options
+        # menu.add label: item[:label], priority: index * 10, **options
+
+        # next if item[:children].blank?
+        # item[:children].each.with_index(1) do |child, child_index|
+        #   child_options = child.except(:children)
+        #   menu.add parent: item[:label], label: child[:label], priority: child_index, **child_options
+        # end
+
+        # menu.add label: item[:label], priority: index * 10, **options do |submenu|
+        #   next if item[:children].blank?
+        #   item[:children].each.with_index(1) do |child, child_index|
+        #     child_options = child.except(:children)
+        #     submenu.add label: child[:label], priority: child_index, **child_options
+        #   end
+        # end
+      end
+    end
+  end
+end
