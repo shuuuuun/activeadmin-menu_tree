5f7bab56fbb07332970a6a57b15c2e24365787b8
shuuuuuny <shuuuuuny@gmail.com>
2021-09-05 12:27:46 +0900

    fix

--- spec/activeadmin/menu_tree_spec.rb
+++ spec/activeadmin/menu_tree_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ActiveAdmin::MenuTree do
     expect(ActiveAdmin::MenuTree::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
