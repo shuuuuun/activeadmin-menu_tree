3a352b0e6cea62c03e59af4a4fd5df669de67ad1
shuuuuuny <shuuuuuny@gmail.com>
2021-09-18 12:59:00 +0900

    load config from yaml

--- spec/dummy/config/initializers/activeadmin-menu_tree.rb
+++ spec/dummy/config/initializers/activeadmin-menu_tree.rb
@@ -1,36 +1,36 @@
 ActiveAdmin::MenuTree.setup do |config|
-  # config.menu_tree = YAML.load_file("_snippets/sample.yml")
-  config.menu_tree = [
-    {
-      name: "Dashboard"
-    }, {
-      label: "User",
-      children: [{
-        name: "User"
-      }]
-    }, {
-      label: "Other",
-      children: [{
-        name: "Foo"
-      }, {
-        name: "Bar"
-      }]
-    }, {
-      label: "Admin",
-      children: [{
-        name: "AdminUser",
-        label: "Admin Users"
-      }, {
-        name: "Comment",
-        label: "Admin Comments",
-        url: "/admin/comments"
-      }]
-    }, {
-      label: "Site",
-      url: "https://example.com",
-      html_options: {
-        target: "blank"
-      }
-    }
-  ]
+  config.menu_tree = YAML.load_file(Rails.root.join("../support/config.yml"))["activeadmin"]["menu_tree"]
+  # config.menu_tree = [
+  #   {
+  #     name: "Dashboard"
+  #   }, {
+  #     label: "User",
+  #     children: [{
+  #       name: "User"
+  #     }]
+  #   }, {
+  #     label: "Other",
+  #     children: [{
+  #       name: "Foo"
+  #     }, {
+  #       name: "Bar"
+  #     }]
+  #   }, {
+  #     label: "Admin",
+  #     children: [{
+  #       name: "AdminUser",
+  #       label: "Admin Users"
+  #     }, {
+  #       name: "Comment",
+  #       label: "Admin Comments",
+  #       url: "/admin/comments"
+  #     }]
+  #   }, {
+  #     label: "Site",
+  #     url: "https://example.com",
+  #     html_options: {
+  #       target: "blank"
+  #     }
+  #   }
+  # ]
 end
