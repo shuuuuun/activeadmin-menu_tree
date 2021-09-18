529b8a316c70def39aa2ae8211197a69055916e1
shuuuuuny <shuuuuuny@gmail.com>
2021-09-18 13:37:04 +0900

    menu_tree

--- spec/dummy/app/admin/profiles.rb
+++ spec/dummy/app/admin/profiles.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Profile do
+  menu_tree
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -14,5 +15,5 @@ ActiveAdmin.register Profile do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
