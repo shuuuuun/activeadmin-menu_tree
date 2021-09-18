d4145e783f55547ef52ff7e720ca96c59780a348
shuuuuuny <shuuuuuny@gmail.com>
2021-09-18 13:36:32 +0900

    seed

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -5,4 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+if Rails.env.development?
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if AdminUser.count.zero?
+  user = User.create!(email: 'user@example.com')
+  Profile.create!(name: 'user name', user: user)
+end
