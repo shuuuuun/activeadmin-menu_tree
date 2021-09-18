dec4a0f1f59a3840585ccc43966254ffc366fc50
shuuuuuny <shuuuuuny@gmail.com>
2021-09-18 16:48:39 +0900

    fix typo

--- spec/dummy/app/admin/dashboard.rb
+++ spec/dummy/app/admin/dashboard.rb
@@ -1,7 +1,6 @@
 ActiveAdmin.register_page "Dashboard" do
   # menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
-  # menu_tree
-  menu_tree lable: 'hello'
+  menu_tree label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
