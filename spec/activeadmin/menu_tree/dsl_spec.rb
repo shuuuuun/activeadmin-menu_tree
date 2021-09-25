d57b9e13faa37941eb1fd5c641af4c77c29fba68
shuuuuuny <shuuuuuny@gmail.com>
2021-09-25 20:55:28 +0900

    refactor using ActiveAdmin::DSL

--- spec/activeadmin/menu_tree/dsl_spec.rb
+++ spec/activeadmin/menu_tree/dsl_spec.rb
@@ -1,24 +1,28 @@
 # frozen_string_literal: true
 
+require "active_admin/dsl"
+
 RSpec.describe ActiveAdmin::MenuTree::DSL do
-  let(:dummy) { Class.new { extend ActiveAdmin::MenuTree::DSL } }
+  ActiveAdmin::DSL.prepend ActiveAdmin::MenuTree::DSL
+
+  let(:dsl) { ActiveAdmin::DSL.new(config) }
+  let(:config) { double("ActiveAdmin::Application") }
 
   describe "menu_tree" do
-    subject { dummy.menu_tree }
+    subject { dsl.menu_tree }
 
-    let(:config) { double("ActiveAdmin::Application") }
     let(:resource_name) { double("ActiveAdmin::Resource::Name") }
 
     before do
       allow(config).to receive(:resource_name).and_return(resource_name)
       allow(resource_name).to receive(:name).and_return("name")
-      allow(dummy).to receive(:config).and_return(config)
-      allow(dummy).to receive(:menu)
+      allow(dsl).to receive(:config).and_return(config)
+      allow(dsl).to receive(:menu)
       allow(ActiveAdmin::MenuTree).to receive(:config).and_call_original
       subject
     end
 
-    it { expect(dummy).to have_received(:menu) }
+    it { expect(dsl).to have_received(:menu) }
     it { expect(ActiveAdmin::MenuTree).to have_received(:config) }
   end
 end
