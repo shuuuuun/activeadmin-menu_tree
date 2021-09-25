79f1da6b8621e8e07ac81539096c276fb2cdc151
shuuuuuny <shuuuuuny@gmail.com>
2021-09-25 18:36:13 +0900

    require active_support/core_ext

--- lib/activeadmin/menu_tree.rb
+++ lib/activeadmin/menu_tree.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require "active_support"
+require "active_support/core_ext"
+
 require_relative "menu_tree/version"
 require_relative "menu_tree/config"
 require_relative "menu_tree/dsl"
