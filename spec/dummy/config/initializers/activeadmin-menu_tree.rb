ee322ac20ec45b27718e9976d81b790b82785328
shuuuuuny <shuuuuuny+git@gmail.com>
2022-02-21 00:12:17 +0900

    chore: add sample menus

--- spec/dummy/config/initializers/activeadmin-menu_tree.rb
+++ spec/dummy/config/initializers/activeadmin-menu_tree.rb
@@ -1,3 +1,4 @@
 ActiveAdmin::MenuTree.setup do |config|
   config.menu_tree = YAML.load_file(Rails.root.join("../support/config.yml"))["activeadmin"]["menu_tree"]
+  # config.menu_tree = YAML.load_file(Rails.root.join("../support/config.yml"))["activeadmin"]["menu_tree_flat"]
 end
